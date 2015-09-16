e7b0a88eef28392e8347bef80ab1f39d6e7492f7
HarrySG <harryhit@gmail.com>
2015-09-16 16:50:23 +0700

    haha

---
+++ todo_user/__openerp__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'Multiuser To-Do',
+    'description': 'Extend the To-Do app to multiuser.',
+    'author': 'Dan',
+    'depends': ['todo_app'],
+    'data': ['todo_view.xml', 'security/todo_access.xml'],
+    }
